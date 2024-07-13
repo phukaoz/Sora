9d864418a70735594a3517bd07fe2da824545c48
U-PHUKAO\tanapat <tanapat.nams@outlook.com>
2024-07-13 22:20:37 +0700

    feat: Log system by spdlog

--- Yuki/src/Yuki/EntryPoint.h
+++ Yuki/src/Yuki/EntryPoint.h
@@ -5,6 +5,10 @@
 extern Yuki::Application* Yuki::CreateApplication();
 
 int main(int argc, char** argv) {
+	Yuki::Log::Init();
+	int a = 10;
+	YUKI_CORE_INFO("my var = {0}", a);
+
 	auto app = Yuki::CreateApplication();
 	app->Run();
 	delete app;
---
+++ Yuki/src/Yuki/Log.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <memory>
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Yuki {
+	class YUKI_API Log{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+
+}
+
+// Core log macros ///////////////////////////////////////////////////////////////////////
+#define YUKI_CORE_TRACE(...)	::Yuki::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define YUKI_CORE_INFO(...)		::Yuki::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define YUKI_CORE_WARN(...)		::Yuki::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define YUKI_CORE_ERROR(...)	::Yuki::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define YUKI_CORE_FATAL(...)	::Yuki::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+//////////////////////////////////////////////////////////////////////////////////////////
+
+// Client log macros /////////////////////////////////////////////////////////////////////
+#define YUKI_TRACE(...)			::Yuki::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define YUKI_INFO(...)			::Yuki::Log::GetClientLogger()->info(__VA_ARGS__)
+#define YUKI_WARN(...)			::Yuki::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define YUKI_ERROR(...)			::Yuki::Log::GetClientLogger()->error(__VA_ARGS__)
+#define YUKI_FATAL(...)			::Yuki::Log::GetClientLogger()->fatal(__VA_ARGS__)
+//////////////////////////////////////////////////////////////////////////////////////////
