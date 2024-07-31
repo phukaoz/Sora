027f9a5210fc6ba08df0ec4f2dd415e4a294039e
Tanapat Namsomboon <tanapat.nams@outlook.com>
2024-07-31 17:37:29 +0700

    style(code): adjust indentation

--- Yuki/src/Yuki/Renderer/VertexArray.cpp
+++ Yuki/src/Yuki/Renderer/VertexArray.cpp
@@ -10,7 +10,7 @@ namespace Yuki {
 	{
 		switch (Renderer::GetAPI())
 		{
-			case RendererAPI::API::None:		YUKI_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+			case RendererAPI::API::None:	YUKI_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
 			case RendererAPI::API::OpenGL:	return new OpenGLVertexArray();
 		}
 
