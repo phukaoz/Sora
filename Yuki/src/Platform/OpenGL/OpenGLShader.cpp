54173f7722d7dd6080e45b7fe29be57991ceced7
Tanapat Namsomboon <tanapat.nams@outlook.com>
2024-07-31 17:33:16 +0700

    refactor(shader): separate shader interface and OpenGL implementation
    - Create shader.h as an interface for shaders
    - Move OpenGL specific implementation to OpenGLShader.h and OpenGLShader.cpp

---
+++ Yuki/src/Platform/OpenGL/OpenGLShader.cpp
@@ -0,0 +1,148 @@
+#include "Yukipch.h"
+#include "OpenGLShader.h"
+
+#include <glad/glad.h>
+                                                                                                                                                                                                                                                                                                                               
+#include <glm/gtc/type_ptr.hpp>
+
+namespace Yuki {
+
+	OpenGLShader::OpenGLShader(const std::string& vertexSrc, const std::string& fragmentSrc)
+	{
+		GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+		const GLchar* source = vertexSrc.c_str();
+		glShaderSource(vertexShader, 1, &source, 0);
+
+		glCompileShader(vertexShader);
+
+		GLint isCompiled = 0;
+		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &isCompiled);
+		if (isCompiled == GL_FALSE)
+		{
+			GLint maxLength = 0;
+			glGetShaderiv(vertexShader, GL_INFO_LOG_LENGTH, &maxLength);
+
+			std::vector<GLchar> infoLog(maxLength);
+			glGetShaderInfoLog(vertexShader, maxLength, &maxLength, &infoLog[0]);
+
+			glDeleteShader(vertexShader);
+
+			YUKI_CORE_ERROR("{0}", infoLog.data());
+			YUKI_CORE_ASSERT(false, "Vertex shader compilation failure!");
+			return;
+		}
+
+		GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+		source = fragmentSrc.c_str();
+		glShaderSource(fragmentShader, 1, &source, 0);
+
+		glCompileShader(fragmentShader);
+
+		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &isCompiled);
+		if (isCompiled == GL_FALSE)
+		{
+			GLint maxLength = 0;
+			glGetShaderiv(fragmentShader, GL_INFO_LOG_LENGTH, &maxLength);
+
+			std::vector<GLchar> infoLog(maxLength);
+			glGetShaderInfoLog(fragmentShader, maxLength, &maxLength, &infoLog[0]);
+
+			glDeleteShader(fragmentShader);
+			glDeleteShader(vertexShader);
+
+			YUKI_CORE_ERROR("{0}", infoLog.data());
+			YUKI_CORE_ASSERT(false, "Fragment shader compilation failure!");
+			return;
+		}
+
+		m_RendererID = glCreateProgram();
+		GLuint program = m_RendererID;
+
+		glAttachShader(program, vertexShader);
+		glAttachShader(program, fragmentShader);
+
+		glLinkProgram(program);
+
+		GLint isLinked = 0;
+		glGetProgramiv(program, GL_LINK_STATUS, (int*)&isLinked);
+		if (isLinked == GL_FALSE)
+		{
+			GLint maxLength = 0;
+			glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
+
+			std::vector<GLchar> infoLog(maxLength);
+			glGetProgramInfoLog(program, maxLength, &maxLength, &infoLog[0]);
+
+			glDeleteProgram(program);
+			glDeleteShader(vertexShader);
+			glDeleteShader(fragmentShader);
+
+			YUKI_CORE_ERROR("{0}", infoLog.data());
+			YUKI_CORE_ASSERT(false, "Shader link failure!");
+			return;
+		}
+
+		glDetachShader(program, vertexShader);
+		glDetachShader(program, fragmentShader);
+	}
+
+	OpenGLShader::~OpenGLShader()
+	{
+		glDeleteProgram(m_RendererID);
+	}
+
+	void OpenGLShader::Bind() const
+	{
+		glUseProgram(m_RendererID);
+	}
+
+	void OpenGLShader::Unbind() const
+	{
+		glUseProgram(0);
+	}
+
+	void OpenGLShader::UploadUniformInt(const std::string& name, int value)
+	{
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform1i(location, value);
+	}
+
+	void OpenGLShader::UploadUniformFloat(const std::string& name, float value)
+	{
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform1f(location, value);
+	}
+
+	void OpenGLShader::UploadUniformFloat2(const std::string& name, const glm::vec2& value)
+	{
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform2f(location, value.x, value.y);
+	}
+
+	void OpenGLShader::UploadUniformFloat3(const std::string& name, const glm::vec3& value)
+	{
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform3f(location, value.x, value.y, value.z);
+	}
+
+	void OpenGLShader::UploadUniformFloat4(const std::string& name, const glm::vec4& value)
+	{
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniform4f(location, value.x, value.y, value.z, value.w);
+	}
+
+	void OpenGLShader::UploadUniformMat3(const std::string& name, const glm::mat3& value)
+	{
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniformMatrix3fv(location, 1, GL_FALSE, glm::value_ptr(value));
+	}
+
+	void OpenGLShader::UploadUniformMat4(const std::string& name, const glm::mat4& value)
+	{
+		GLint location = glGetUniformLocation(m_RendererID, name.c_str());
+		glUniformMatrix4fv(location, 1, GL_FALSE, glm::value_ptr(value));
+	}
+
+}
