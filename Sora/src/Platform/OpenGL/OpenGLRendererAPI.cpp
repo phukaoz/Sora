587068e64ffeb83a878925e095a52a011a62db87
Tanapat Namsomboon <tanapat.nams@outlook.com>
2024-10-01 05:12:41 +0700

    feat(OpenGL): enable line smooth

--- Sora/src/Platform/OpenGL/OpenGLRendererAPI.cpp
+++ Sora/src/Platform/OpenGL/OpenGLRendererAPI.cpp
@@ -13,7 +13,9 @@ namespace Sora {
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 		glEnable(GL_DEPTH_TEST);
-		glDepthFunc(GL_LEQUAL);
+		//glDepthFunc(GL_LEQUAL);
+
+		glEnable(GL_LINE_SMOOTH);
 	}
 
 	void OpenGLRendererAPI::SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height)
