a3cb683dee3ed901e1b7710fe587faf788f32c19
Tanapat Namsomboon <tanapat.nams@outlook.com>
2024-10-14 21:16:16 +0700

    feat(vendor): added mono

--- Sora/premake5.lua
+++ Sora/premake5.lua
@@ -42,7 +42,8 @@ project "Sora"
 		"%{IncludeDir.yaml_cpp}",
 		"%{IncludeDir.ImGuizmo}",
 		"%{IncludeDir.VulkanSDK}",
-		"%{IncludeDir.box2d}"
+		"%{IncludeDir.box2d}",
+		"%{IncludeDir.mono}",
 	}
 
 	links
@@ -53,10 +54,8 @@ project "Sora"
 		"yaml-cpp",
 		"opengl32.lib",
 		"box2d",
-		"d3d11.lib",
-		"dxgi.lib",
-		"d3dcompiler.lib",
-		"dxguid.lib"
+		
+		"%{Library.mono}",
 	}
 	
 	filter "files:vendor/ImGuizmo/**.cpp"
