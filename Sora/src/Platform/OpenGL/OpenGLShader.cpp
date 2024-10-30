424cca1c7c647ea80687ff3c3b30b8920022d2ea
Tanapat Namsomboon <tanapat.nams@outlook.com>
2024-10-30 15:04:45 +0700

    refactor(shader): changed variable's name

--- Sora/src/Platform/OpenGL/OpenGLShader.cpp
+++ Sora/src/Platform/OpenGL/OpenGLShader.cpp
@@ -113,11 +113,11 @@ namespace Sora {
 		lastSlash = lastSlash == std::string::npos ? 0 : lastSlash + 1;
 		auto lastDot = filepath.rfind('.');
 		auto count = lastDot == std::string::npos ? filepath.size() - lastSlash : lastDot - lastSlash;
-		mName = filepath.substr(lastSlash, count);
+		m_Name = filepath.substr(lastSlash, count);
 	}
 
 	OpenGLShader::OpenGLShader(const std::string& name, const std::string& vertexSrc, const std::string& fragmentSrc)
-		: mName(name)
+		: m_Name(name)
 	{
 		SORA_PROFILE_FUNCTION();
 
