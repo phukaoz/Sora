7b5a66c9db31b940a8cd90894023a8a46acfb04e
Tanapat Namsomboon <tanapat.nams@outlook.com>
2024-10-30 14:56:20 +0700

    fix(shader): fixed bug
    fixed some bug that make textures look weird when on release mode.

--- SoraEditor/assets/shaders/Renderer2D_Line.glsl
+++ SoraEditor/assets/shaders/Renderer2D_Line.glsl
@@ -3,7 +3,7 @@
 
 layout(location = 0) in vec3 a_Position;
 layout(location = 1) in vec4 a_Color;
-layout(location = 2) in int a_EntityID;
+layout(location = 2) in int  a_EntityID;
 
 layout(std140, binding = 0) uniform Camera
 {
@@ -16,14 +16,14 @@ struct VertexOutput
 };
 
 layout (location = 0) out VertexOutput Output;
-layout (location = 1) out flat int v_EntityID;
+layout (location = 4) out flat int v_EntityID;
 
 void main()
 {
 	Output.Color = a_Color;
-	v_EntityID = a_EntityID;
+	v_EntityID   = a_EntityID;
 
-	gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+	gl_Position  = u_ViewProjection * vec4(a_Position, 1.0);
 }
 
 #type fragment
